759bdff792ef867582d3a3295727a1ad7e49ef9d
Dani <155178581+danifer-svg@users.noreply.github.com>
2024-05-07 19:54:44 -0300

    Update db_agendamento_vet.sql

--- db_agendamento_vet.sql
+++ db_agendamento_vet.sql
@@ -1,24 +1,13 @@
 CREATE TABLE db_agendamento_vet.CADASTRO (
 	ID_CADASTRO int AUTO_INCREMENT NOT NULL UNIQUE,
 	nome varchar(150) NOT NULL,
-	email varchar(250) NOT NULL,
+    	cpf int(11) NOT NULL,
 	fone varchar(15) NOT NULL,
-	cpf int NOT NULL,
-	endereco varchar(250) NOT NULL,
-	PRIMARY KEY (ID_CADASTRO)
-);
-
-CREATE TABLE db_agendamento_vet.INFORMACOESANIMAL (
-	ID_INF int AUTO_INCREMENT NOT NULL UNIQUE,
 	nome_pet varchar(150) NOT NULL,
-	especie varchar(100) NOT NULL,
-	raca varchar(100) NOT NULL,
-	idade varchar(10) NOT NULL,
+	raca_pet varchar(100) NOT NULL,
+	idade_pet varchar(10) NOT NULL,
 	descricao varchar(2000) NOT NULL,
 	data_consulta date NOT NULL,
-	id_cadastro int NOT NULL,
-	PRIMARY KEY (ID_INF)
+    	hora_consulta time NOT NULL,
+	PRIMARY KEY (ID_CADASTRO)
 );
-
-
-ALTER TABLE db_agendamento_vet.INFORMACOESANIMAL ADD CONSTRAINT INFORMACOESANIMAL_fk7 FOREIGN KEY (ID_CADASTRO) REFERENCES db_agendamento_vet.CADASTRO(ID_CADASTRO);
