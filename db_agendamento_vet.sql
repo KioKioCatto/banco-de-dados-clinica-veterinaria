3d4c03c04788928be835798aa52ef18bc1817cc7
Dani <155178581+danifer-svg@users.noreply.github.com>
2024-06-10 16:23:45 -0300

    Update db_agendamento_vet.sql

--- db_agendamento_vet.sql
+++ db_agendamento_vet.sql
@@ -1,11 +1,11 @@
 CREATE TABLE consulta (
 	id_consulta int NOT NULL,
 	nome varchar2(250) NOT NULL,
-    cpf number(11) NOT NULL,
+    	cpf number(11) NOT NULL,
 	nome_pet varchar2(150) NOT NULL,
 	especie_pet varchar2(150) NOT NULL,
 	data_consulta date NOT NULL,
-    hora_consulta TIMESTAMP NOT NULL,
+    	hora_consulta TIMESTAMP NOT NULL,
 	constraint pk_id_consulta primary key(id_consulta)
 );
 
