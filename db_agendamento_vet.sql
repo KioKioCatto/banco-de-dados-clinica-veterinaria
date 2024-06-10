c121c92eea9f827fab6ad2202cf0c7e1a5c7f586
Dani <155178581+danifer-svg@users.noreply.github.com>
2024-06-10 16:23:28 -0300

    Update db_agendamento_vet.sql

--- db_agendamento_vet.sql
+++ db_agendamento_vet.sql
@@ -1,13 +1,14 @@
-CREATE TABLE db_agendamento_vet.CADASTRO (
-	ID_CADASTRO int AUTO_INCREMENT NOT NULL UNIQUE,
-	nome varchar(150) NOT NULL,
-    	cpf int(11) NOT NULL,
-	fone varchar(15) NOT NULL,
-	nome_pet varchar(150) NOT NULL,
-	raca_pet varchar(100) NOT NULL,
-	idade_pet varchar(10) NOT NULL,
-	descricao varchar(2000) NOT NULL,
+CREATE TABLE consulta (
+	id_consulta int NOT NULL,
+	nome varchar2(250) NOT NULL,
+    cpf number(11) NOT NULL,
+	nome_pet varchar2(150) NOT NULL,
+	especie_pet varchar2(150) NOT NULL,
 	data_consulta date NOT NULL,
-    	hora_consulta time NOT NULL,
-	PRIMARY KEY (ID_CADASTRO)
+    hora_consulta TIMESTAMP NOT NULL,
+	constraint pk_id_consulta primary key(id_consulta)
 );
+
+select *from consulta
+
+commit
